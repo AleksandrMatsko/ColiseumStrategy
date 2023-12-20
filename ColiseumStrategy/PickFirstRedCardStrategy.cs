b5962430c9d324ab6fc8bfc20a67825cb8024c6a
Aleksandr Matsko <a.matsko@g.nsu.ru>
2023-12-20 20:24:02 +0700

    chore: initial

---
+++ ColiseumStrategy/PickFirstRedCardStrategy.cs
@@ -0,0 +1,20 @@
+﻿using Nsu.ColiseumProblem.Contracts;
+using Nsu.ColiseumProblem.Contracts.Cards;
+
+namespace ColiseumStrategy;
+
+public class PickFirstRedCardStrategy : ICardPickStrategy
+{
+    public int Pick(Card[] cards)
+    {
+        for (var i = 0; i < cards.Length; i++)
+        {
+            if (cards[i].Color == CardColor.Red)
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
